f772cda4fb81bd82e44b6e229e0ffec91666914a
Pabolo Pires <pabolo18@gmail.com>
2021-09-12 14:27:45 -0300

    Versão inicial do LA sintático

---
+++ trabalho2/la-sintatico/src/main/antlr4/br/ufscar/dc/compiladores/la/sintatico/LA.g4
@@ -0,0 +1,5 @@
+grammar LA;
+
+hello:
+    'world'
+    ;
---
+++ trabalho2/la-sintatico/src/main/java/br/ufscar/dc/compiladores/la/sintatico/Principal.java
@@ -0,0 +1,7 @@
+package br.ufscar.dc.compiladores.la.sintatico;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
