2ec6d75892fde534d99cb1783fd0c4e181f5d48f
Vanessa-webusr <alvesnessa42@gmail.com>
2021-09-12 19:57:59 -0300

    Comentarios de criacao de mensagem

--- trabalho2/la-sintatico/src/main/java/br/ufscar/dc/compiladores/la/sintatico/CustomErrorListener.java
+++ trabalho2/la-sintatico/src/main/java/br/ufscar/dc/compiladores/la/sintatico/CustomErrorListener.java
@@ -9,8 +9,10 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 import java.io.PrintWriter;
 import java.util.BitSet;
 
+//Classe responsável por customizar as mensagens de erro
 public class CustomErrorListener implements ANTLRErrorListener {
 
+    //Variável para escrever no arquivo .txt
     PrintWriter pw;
 
     public CustomErrorListener(PrintWriter pw) {
@@ -23,18 +25,21 @@ public class CustomErrorListener implements ANTLRErrorListener {
         String message;
         String tokenTypeName = LALexer.VOCABULARY.getDisplayName(t.getType());
 
+        //Verifica se o erro é devido a um caractere desconhecido
         if (tokenTypeName.equals("DESCONHECIDO")) {
             String invalidChar = t.getText();
+
+            //Erro devido ao não fechamento de comentário
             if (invalidChar.equals("{")) {
                 message = "Linha " + line + ": comentario nao fechado";
-            } else if (invalidChar.equals("\"")) {
+            } else if (invalidChar.equals("\"")) {      //Erro devido ao não fechamento de cadeia literal
                 message = "Linha " + line + ": cadeia literal nao fechada";
-            } else {
+            } else {        //Erro devido ao não reconhecimento de símbolos
                 message = "Linha " + line + ": " + t.getText() + " - simbolo nao identificado";
             }
-        } else if (t.getType() == Token.EOF) {
+        } else if (t.getType() == Token.EOF) {      //Erro devido à não finalização do algoritmo
             message = "Linha " + line + ": erro sintatico proximo a EOF";
-        } else {
+        } else {        //Erro sintático
             message = "Linha " + line + ": erro sintatico proximo a " + t.getText();
         }
 
